facb3992dd753e8288367655dc08a0b941f2a15e
Mtn-Drew <drewc3@gmail.com>
2020-07-27 05:08:25 -0600

    33:50

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-const express = require('express')
+const express = require("express")
+const mongoose = require("mongoose")
+const Article = require('./models/article')
+const articleRouter = require("./routes/articles")
+
 const app = express()
 
-app.set('view engine', 'ejs')
-
-app.get('/', (req,res)=>{
-  res.send('Hello World!!?')
+mongoose.connect("mongodb://localhost/blog", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 })
 
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false }))
 
+app.get("/", (req, res) => {
+	const articles = [
+		{
+			title: "Test Article",
+			createdAt: new Date(),
+			description: "Test description",
+		},
+		{
+			title: "Test Article2",
+			createdAt: new Date(),
+			description: "Test description2",
+		},
+	]
+	res.render("articles/index", { articles: articles })
+})
 
+app.use("/articles", articleRouter)
 
-app.listen(5000)
-
+app.listen(5001)
